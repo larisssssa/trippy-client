e090ae61ed44ee58a98ac90e82166abd990eaefb
Larissa Ferreira <larissadferr@gmail.com>
2026-05-13 10:17:40 -0500

    navbar: converted to jsx and using bulma

---
+++ components/navbar.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { useAppContext } from "../context/state";
+
+export default function Navbar() {
+  const { token } = useAppContext();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, [token]);
+
+  return (
+    <>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item" href="">
+              Home
+            </a>
+            <a class="navbar-item" href="my-trips">
+              My Trips
+            </a>
+          </div>
+          <div class="navbar-end">
+            <div class="navbar-item">
+              {isLoggedIn ? (
+                <a
+                  class="button is-light"
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    setIsLoggedIn(false);
+                  }}
+                >
+                  Log out
+                </a>
+              ) : (
+                <div class="buttons">
+                  <a class="button is-primary" href="register">
+                    <strong>Sign up</strong>
+                  </a>
+                  <a class="button is-light" href="login">
+                    Log in
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
