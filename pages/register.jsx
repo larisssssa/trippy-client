d9f1aac42b33fe3069de77b5224b9e0ed1fe548f
Larissa Ferreira <larissadferr@gmail.com>
2026-05-13 14:21:48 -0500

    pages/register: updated to react component

---
+++ pages/register.jsx
@@ -0,0 +1,84 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { Input } from "../components/form-elements/input";
+import Layout from "../components/layout";
+import Navbar from "../components/navbar";
+import { useAppContext } from "../context/state";
+import { register } from "../data/auth";
+
+export default function Register() {
+  const { setToken } = useAppContext();
+
+  const firstName = useRef("");
+  const lastName = useRef("");
+  const username = useRef("");
+  const password = useRef("");
+  const email = useRef("");
+
+  const router = useRouter();
+
+  const submit = (e) => {
+    e.preventDefault();
+
+    const user = {
+      username: username.current.value,
+      password: password.current.value,
+      first_name: firstName.current.value,
+      last_name: lastName.current.value,
+      email: email.current.value,
+    };
+
+    register(user).then((res) => {
+      if (res.token) {
+        setToken(res.token);
+        router.push("/");
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div>
+        <form>
+          <h1>Welcome!</h1>
+          <Input
+            id="firstName"
+            refEl={firstName}
+            type="text"
+            label="First Name"
+          />
+          <Input id="lastName" refEl={lastName} type="text" label="Last Name" />
+          <Input id="email" refEl={email} type="text" label="Email" />
+          <Input id="username" refEl={username} type="text" label="Username" />
+          <Input
+            id="password"
+            refEl={password}
+            type="password"
+            label="Password"
+          />
+
+          <div>
+            <div>
+              <button onClick={submit}>Submit</button>
+            </div>
+            <div>
+              <Link href="/login">
+                <button>Cancel</button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+Register.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <Navbar />
+      {page}
+    </Layout>
+  );
+};
