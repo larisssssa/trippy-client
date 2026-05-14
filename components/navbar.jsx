f8e1a9804ca19edbced8edd3c22913a38ee48e22
Larissa Ferreira <larissadferr@gmail.com>
2026-05-13 19:27:10 -0500

    component/navbar: updated Home page link

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -20,7 +20,7 @@ export default function Navbar() {
       >
         <div class="navbar-menu is-active">
           <div class="navbar-start">
-            <a class="navbar-item" href="">
+            <a class="navbar-item" href="home">
               Home
             </a>
             <a class="navbar-item" href="my-trips">
