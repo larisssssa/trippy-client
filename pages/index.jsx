2404766016a7f698e3b3d7b223083ee2f3cc8c6a
Larissa Ferreira <larissadferr@gmail.com>
2026-05-14 11:05:24 -0500

    pages: react

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Layout from "../components/layout";
+import Navbar from "../components/navbar";
+import Home from "./home";
+
+export default function Index() {
+  return <Home />;
+}
+
+Index.getLayout = function getLayout(page) {
+  return (
+    <Layout>
+      <Navbar />
+      {page}
+    </Layout>
+  );
+};
