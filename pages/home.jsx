1d85bd5d999e6b29a1e7e662694b2c0bff6bc68c
Larissa Ferreira <larissadferr@gmail.com>
2026-05-14 11:02:08 -0500

    pages/home: created temp testing component

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import Layout from "../components/layout";
 import Navbar from "../components/navbar";
+import { TripCard } from "../components/trip/card";
+import { getTrips } from "../data/trips";
 
 export default function Home() {
-  return <></>;
+  const [trips, setTrips] = useState([]);
+  useEffect(() => {
+    getTrips().then((trips) => {
+      setTrips(trips);
+    });
+  }, []);
+
+  return (
+      <div className="container">
+        <header className="header">Header</header>
+        <div class="columns ">
+          {trips.map((trip) => (
+            <TripCard trip={trip} key={trip.id} />
+          ))}
+        </div>
+      </div>
+  );
 }
 Home.getLayout = function getLayout(page) {
   return (
