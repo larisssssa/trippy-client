ea3c95115facd0e64d7377433cd61cf02f92e0fc
Larissa Ferreira <larissadferr@gmail.com>
2026-05-14 10:59:39 -0500

    removed

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../globals.css";
-
-export default function Trippy({ Component, pageProps }) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page);
-
-  return getLayout(<Component {...pageProps} />);
-}
