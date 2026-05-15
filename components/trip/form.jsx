e0ab09ed2921c9b70dc72b9194fc3306c7dfb746
Larissa Ferreira <larissadferr@gmail.com>
2026-05-15 17:24:13 -0500

    added handlers for saving trip and creating trip user

--- components/trip/form.jsx
+++ components/trip/form.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getUser } from "../../data/auth";
+import { createNewTrip, createNewTripUser } from "../../data/trips";
+import { useRouter } from "next/router";
 
 export function NewTripForm() {
+  const [user, setUser] = useState(0);
   const [trip, setTrip] = useState({
     name: "",
     destination: "",
@@ -8,17 +12,47 @@ export function NewTripForm() {
     departure_date: "",
     return_date: "",
     imageurl: "",
+    creator: "",
   });
+  const router = useRouter();
+
+  useEffect(() => {
+    getUser().then((data) => {
+      setUser(data);
+    });
+  }, []);
 
   const updateTrip = (e) => {
     const copy = { ...trip };
+    if (copy.creator == "") {
+      copy.creator = user.id;
+    }
     copy[e.target.id] = e.target.value;
     setTrip(copy);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createNewTrip(trip).then((res) => {
+      if (!res || !res.id) {
+        return;
+      } else {
+        addUserToTrip(res.id);
+      }
+      router.push("/trips");
+    });
+  };
+
+  const addUserToTrip = (id) => {
+    const username = { username: user.username };
+    createNewTripUser(id, username);
+  };
+
   return (
     <>
       <div class="section">
         <form class="box">
+          <p class="title has-text-centered">Create a new trip!</p>
           <div class="field">
             <label class="label">Trip Name</label>
             <div class="control">
@@ -27,6 +61,7 @@ export function NewTripForm() {
                 id="name"
                 type="text"
                 onChange={updateTrip}
+                required
               />
             </div>
           </div>
@@ -38,6 +73,7 @@ export function NewTripForm() {
                 id="destination"
                 type="text"
                 onChange={updateTrip}
+                required
               />
             </div>
           </div>
@@ -49,17 +85,6 @@ export function NewTripForm() {
                 id="country"
                 type="text"
                 onChange={updateTrip}
-              />
-            </div>
-          </div>
-          <div class="field">
-            <label class="label">Destination</label>
-            <div class="control">
-              <input
-                class="input"
-                id="destination"
-                type="text"
-                onChange={updateTrip}
                 required
               />
             </div>
@@ -69,7 +94,7 @@ export function NewTripForm() {
             <div class="control">
               <input
                 class="input"
-                id="departure"
+                id="departure_date"
                 type="date"
                 onChange={updateTrip}
                 required
@@ -81,7 +106,7 @@ export function NewTripForm() {
             <div class="control">
               <input
                 class="input"
-                id="return"
+                id="return_date"
                 type="date"
                 onChange={updateTrip}
                 required
@@ -89,11 +114,11 @@ export function NewTripForm() {
             </div>
           </div>
           <div class="field">
-            <label class="label">Image</label>
+            <label class="label">Image link</label>
             <div class="control">
               <input
                 class="input"
-                id="image"
+                id="imageurl"
                 type="text"
                 onChange={updateTrip}
                 required
@@ -102,7 +127,9 @@ export function NewTripForm() {
           </div>
           <div class="field is-grouped">
             <div class="control">
-              <button class="button is-link">Create Trip</button>
+              <button class="button is-link" onClick={handleSubmit}>
+                Create Trip
+              </button>
             </div>
             <div class="control">
               <a href="/home" class="button is-link">
