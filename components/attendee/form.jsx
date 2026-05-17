7ccdbc66f30ae5b862535faf56ebead662ce5976
Larissa Ferreira <larissadferr@gmail.com>
2026-05-17 12:39:46 -0500

    added handle submit for adding attendee

--- components/attendee/form.jsx
+++ components/attendee/form.jsx
@@ -1,4 +1,31 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { createNewTripUser } from "../../data/trips";
+
 export function NewAttendeeForm() {
+  const router = useRouter();
+  const [attendee, setAttendee] = useState({
+    username: "",
+  });
+  const [error, setError] = useState("");
+  const { id } = router.query;
+
+  const updateAttendee = (e) => {
+    const copy = { ...attendee };
+    copy[e.target.id] = e.target.value;
+    setAttendee(copy);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createNewTripUser(id, attendee).then((res) => {
+      if (res) {
+        setError("User does not exist");
+      } else {
+        router.push(`/trips/${id}`);
+      }
+    });
+  };
+
   return (
     <>
       <div class="section">
@@ -7,13 +34,22 @@ export function NewAttendeeForm() {
           <div class="field">
             <label class="label">Username</label>
             <div class="control">
-              <input class="input" id="username" type="text" required />
+              <input
+                class="input"
+                id="username"
+                type="text"
+                onChange={updateAttendee}
+                required
+              />
             </div>
           </div>
           <div class="field is-grouped">
             <div class="control">
-              <button class="button is-link">Invite</button>
+              <button class="button is-link" onClick={handleSubmit}>
+                Invite
+              </button>
             </div>
+            <p class="help is-danger">{error}</p>
           </div>
         </form>
       </div>
