f3e92b59b398297bc9aa7e2be719b4f9efee6b7b
Larissa Ferreira <larissadferr@gmail.com>
2026-05-17 15:09:31 -0500

    added category select dropdown to attraction form

--- components/attraction/form.jsx
+++ components/attraction/form.jsx
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+import { getCategories } from "../../data/attractions";
+
 export function NewAttractionForm() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((res) => setCategories(res));
+  }, []);
+
   return (
     <>
       <div class="section">
@@ -6,37 +15,46 @@ export function NewAttractionForm() {
           <div class="field">
             <label class="label">Location Name</label>
             <div class="control">
-              <input id="name" type="text" />
+              <input id="name" type="text" class="input" required />
             </div>
           </div>
           <div class="field">
             <label class="label">Description</label>
             <div class="control">
-              <input id="description" type="text" />
+              <input id="description" type="text" class="input" required />
             </div>
           </div>
           <div class="field">
             <label class="label">City</label>
             <div class="control">
-              <input id="city" type="text" />
+              <input id="city" type="text" class="input" required />
             </div>
           </div>
           <div class="field">
             <label class="label">Country</label>
             <div class="control">
-              <input id="country" type="text" />
+              <input id="country" type="text" class="input" required />
             </div>
           </div>
           <div class="field">
             <label class="label">Image link</label>
             <div class="control">
-              <input id="imageurl" type="text" />
+              <input id="imageurl" type="text" class="input" required />
             </div>
           </div>
           <div class="field">
             <label class="label">Category</label>
-            <div class="control">
-              <input id="category" type="text" />
+            <div class="select">
+              <select>
+                <option key="0" value="0">
+                  - Select Category -
+                </option>
+                {categories.map((cat) => (
+                  <option key={cat.id} value={cat.name}>
+                    {cat.name}
+                  </option>
+                ))}
+              </select>
             </div>
           </div>
         </form>
