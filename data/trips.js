9bcc091a2e9b40e6f0a580cac77b8263e273fbf8
Larissa Ferreira <larissadferr@gmail.com>
2026-05-17 17:19:24 -0500

    added remove trip attendee request

--- data/trips.js
+++ data/trips.js
@@ -66,3 +66,14 @@ export function addAttraction(id, attraction) {
     body: JSON.stringify(attraction),
   });
 }
+
+export function removeTripAttendee(id, attendee) {
+  return fetchWithoutResponse(`trips/${id}/attendee`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Token ${localStorage.getItem("token")}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(attendee),
+  });
+}
